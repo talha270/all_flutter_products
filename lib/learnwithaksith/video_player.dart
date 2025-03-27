37d7ad010d4b084008ac86e61278eb5b0c72a2fd
talha270 <144779558+talha270@users.noreply.github.com>
2025-03-27 22:14:14 +0500

    added

--- lib/learnwithaksith/video_player.dart
+++ lib/learnwithaksith/video_player.dart
@@ -1,119 +1,119 @@
-import 'package:flutter/material.dart';
-import 'package:appinio_video_player/appinio_video_player.dart';
-// enum Source{
-//   Assets,
-//   Network
+// import 'package:flutter/material.dart';
+// import 'package:appinio_video_player/appinio_video_player.dart';
+// // enum Source{
+// //   Assets,
+// //   Network
+// // }
+// class VideoPlayerPackage extends StatefulWidget {
+//   const VideoPlayerPackage({super.key});
+//
+//   @override
+//   State<VideoPlayerPackage> createState() => _VideoPlayerPackageState();
 // }
-class VideoPlayerPackage extends StatefulWidget {
-  const VideoPlayerPackage({super.key});
-
-  @override
-  State<VideoPlayerPackage> createState() => _VideoPlayerPackageState();
-}
-
-class _VideoPlayerPackageState extends State<VideoPlayerPackage> {
-  // Source currentsource=Source.Assets;
-
-  late CustomVideoPlayerController _customVideoPlayerController;
-  late CachedVideoPlayerController _videoPlayerController; // Moved outside of the method
-  String assetPath = "assets/videos/movie.mp4";
-  String url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-
-  @override
-  void initState() {
-    super.initState();
-    initializeVideoPlayer("asset");
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    _customVideoPlayerController.dispose();
-    super.dispose();
-  }
-
-  void initializeVideoPlayer(String source) {
-
-    if(source=="asset"){
-      _videoPlayerController=CachedVideoPlayerController.asset(assetPath)..initialize().then((value){
-        setState(() {
-
-        });
-      });
-    }else if(source=="network"){
-      _videoPlayerController=CachedVideoPlayerController.network(url)..initialize().then((onValue){
-        setState(() {
-
-        });
-      });
-    }else{
-      return;
-    }
-    // _videoPlayerController = CachedVideoPlayerController.asset(assetPath)
-    //   ..initialize().then((_) {
-    //     setState(() {}); // Ensure the UI is updated after initialization
-    //   }).catchError((error) {
-    //     print("Error initializing video player: $error");
-    //   });
-
-    _customVideoPlayerController = CustomVideoPlayerController(
-      context: context,
-      videoPlayerController: _videoPlayerController,
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Video Player"),
-        centerTitle: true,
-        elevation: 20,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _videoPlayerController.value.isInitialized
-              ? SizedBox(
-            height: 250,
-            width: double.infinity,
-            child: CustomVideoPlayer(
-              customVideoPlayerController: _customVideoPlayerController,
-            ),
-          )
-              : CircularProgressIndicator(),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(onPressed: (){
-              initializeVideoPlayer("asset");
-              },
-                  child: Text("Assets")),
-              ElevatedButton(onPressed: (){
-              initializeVideoPlayer("network");
-                },
-                  child: Text("Network")),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-  // void enterfullscreen(){
-  //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  //   SystemChrome.setPreferredOrientations([
-  //     DeviceOrientation.landscapeLeft,
-  //     DeviceOrientation.landscapeRight
-  //   ]);
-  // }
-  // void exitfullscreen(){
-  //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
-  //   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  // }
+//
+// class _VideoPlayerPackageState extends State<VideoPlayerPackage> {
+//   // Source currentsource=Source.Assets;
+//
+//   late CustomVideoPlayerController _customVideoPlayerController;
+//   late CachedVideoPlayerController _videoPlayerController; // Moved outside of the method
+//   String assetPath = "assets/videos/movie.mp4";
+//   String url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     initializeVideoPlayer("asset");
+//   }
+//
+//   @override
+//   void dispose() {
+//     _videoPlayerController.dispose();
+//     _customVideoPlayerController.dispose();
+//     super.dispose();
+//   }
+//
+//   void initializeVideoPlayer(String source) {
+//
+//     if(source=="asset"){
+//       _videoPlayerController=CachedVideoPlayerController.asset(assetPath)..initialize().then((value){
+//         setState(() {
+//
+//         });
+//       });
+//     }else if(source=="network"){
+//       _videoPlayerController=CachedVideoPlayerController.network(url)..initialize().then((onValue){
+//         setState(() {
+//
+//         });
+//       });
+//     }else{
+//       return;
+//     }
+//     // _videoPlayerController = CachedVideoPlayerController.asset(assetPath)
+//     //   ..initialize().then((_) {
+//     //     setState(() {}); // Ensure the UI is updated after initialization
+//     //   }).catchError((error) {
+//     //     print("Error initializing video player: $error");
+//     //   });
+//
+//     _customVideoPlayerController = CustomVideoPlayerController(
+//       context: context,
+//       videoPlayerController: _videoPlayerController,
+//     );
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Video Player"),
+//         centerTitle: true,
+//         elevation: 20,
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           _videoPlayerController.value.isInitialized
+//               ? SizedBox(
+//             height: 250,
+//             width: double.infinity,
+//             child: CustomVideoPlayer(
+//               customVideoPlayerController: _customVideoPlayerController,
+//             ),
+//           )
+//               : CircularProgressIndicator(),
+//           SizedBox(
+//             height: 20,
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               ElevatedButton(onPressed: (){
+//               initializeVideoPlayer("asset");
+//               },
+//                   child: Text("Assets")),
+//               ElevatedButton(onPressed: (){
+//               initializeVideoPlayer("network");
+//                 },
+//                   child: Text("Network")),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
+//
+//   // void enterfullscreen(){
+//   //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+//   //   SystemChrome.setPreferredOrientations([
+//   //     DeviceOrientation.landscapeLeft,
+//   //     DeviceOrientation.landscapeRight
+//   //   ]);
+//   // }
+//   // void exitfullscreen(){
+//   //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+//   //   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+//   // }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import shared_preferences_foundation
 import sqflite
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -31,4 +32,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
